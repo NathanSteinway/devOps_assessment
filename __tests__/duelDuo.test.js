4e0a779656f1e290b5b47cda4360ee404686af4e
ganbari <steinway.nathan@gmail.com>
2023-06-24 19:18:11 -0500

    completes part 4

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,40 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+
+  test('Clicking Draw button displays div w/ id of choices', async () => {
+
+    await driver.get("http://localhost:8000/")
+    
+    let drawBtn = await driver.findElement(By.id("draw"))
+
+    await drawBtn.click()
+
+    await driver.sleep(3000)
+
+    let showDraw = await driver.findElement(By.id("choices"))
+
+    expect(showDraw).not.toHaveProperty("hide")
+
+  })
+
+  test('Clicking Add to Duo button displays the div / id = "player-duo', async () => {
+
+    await driver.get("http://localhost:8000/")
+
+    // How to reference a dynamically generated class?
+    // Keeps saying im using css selector method for some reason
+    let duoAddBtn = await driver.findElement(By.className("bot-btn"));
+
+    await driver.sleep(3000)
+
+    await duoAddBtn.click()
+
+    await driver.sleep(3000)
+
+    let playerDuoDiv = await driver.findElement(By.id("player-duo"))
+
+    expect(playerDuoDiv).not.toHaveProperty("hide")
+
+  }) 
 });
